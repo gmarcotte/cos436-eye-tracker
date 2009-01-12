54746595c5567c6ffb4fc3eb4feddd43e4429e54
garrett.marcotte <garrett.marcotte@45935db4-cf16-11dd-8ca8-e3ff79f713b6>
2009-01-12 05:56:04 +0000

    Adding enum for available actions.

--- trunk/Recognition/Recognition/EyeTracker/config.h
+++ trunk/Recognition/Recognition/EyeTracker/config.h
@@ -1,9 +1,30 @@
 /* Configuration settings for the eye tracking application */
 
+
+// Tracking configurations
 enum {
 	NUM_INPUTS				= 2,
 	MAX_STRING				= 255,
 	FLIP_INPUT				= 1,
 	MIN_PUPIL_RADIUS  = 5,
 	MIN_EB_PERIM			= 100,
+	CANNY_LOW					= 200,
+	CANNY_HIGH				= 250,
 };
+
+
+// Available actions
+enum {
+	EYE_LEFT,
+	EYE_RIGHT,
+	EYE_UP,
+	EYE_DOWN,
+	LEFT_WINK,
+	RIGHT_WINK,
+	BLINK,
+	EYEBROW_RAISE,
+	NUM_ACTIONS
+}
+
+
+
